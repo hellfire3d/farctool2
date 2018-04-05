6bbff83d1b66705060ab7271d46ec4058434cc19
philosophofee <>
2018-04-05 08:38:56 -0400

    first commit

---
+++ farctool2/src/com/philosophofee/farctool2/MainWindow.java
@@ -0,0 +1,199 @@
+package com.philosophofee.farctool2;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+public class MainWindow extends javax.swing.JFrame {
+
+    public MainWindow() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        fileChooser = new javax.swing.JFileChooser();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        mapTree = new javax.swing.JTree();
+        jPanel2 = new javax.swing.JPanel();
+        testButton = new javax.swing.JButton();
+        mapLoadingBar = new javax.swing.JProgressBar();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        Open = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("farctool2");
+        setResizable(false);
+
+        mapTree.setModel(null);
+        jScrollPane1.setViewportView(mapTree);
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        testButton.setText("test");
+        testButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                testButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(testButton)
+                .addContainerGap(224, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(testButton)
+                .addContainerGap(217, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        jMenu1.setText("File");
+
+        Open.setText("Open");
+        Open.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OpenActionPerformed(evt);
+            }
+        });
+        jMenu1.add(Open);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(mapLoadingBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(mapLoadingBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    
+    
+    private void testButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_testButtonActionPerformed
+        //MapParser self = new MapParser();
+        //self.addPath("gamedata/models/mol03.mol");
+        //self.printHtml(System.out);
+    }//GEN-LAST:event_testButtonActionPerformed
+
+    private void OpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OpenActionPerformed
+        FileFilter ff = new FileFilter(){
+            public boolean accept(File f){
+                if(f.isDirectory()) return true;
+                else if(f.getName().endsWith(".map")) return true;
+                    else return false;
+            }
+            public String getDescription(){
+                return "Map Files";
+            }
+        };
+        fileChooser.removeChoosableFileFilter(fileChooser.getAcceptAllFileFilter());
+        fileChooser.setFileFilter(ff);
+        int returnVal = fileChooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            System.out.println("Sucessfully opened " + file.getName());
+            MapParser self = new MapParser();
+            DefaultMutableTreeNode root = new DefaultMutableTreeNode(file.getName());
+            mapTree.setModel(null);
+            DefaultTreeModel model = self.parseMapIntoMemory(root, file);
+
+            mapTree.setModel(model);
+            //self.loadMap(file);
+            //self.printHtml(System.out);
+        } else {
+            System.out.println("File access cancelled by user.");
+        }
+    }//GEN-LAST:event_OpenActionPerformed
+
+    public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Metal".equals(info.getName())) {
+                    //javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+                    
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem Open;
+    private javax.swing.JFileChooser fileChooser;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JProgressBar mapLoadingBar;
+    private javax.swing.JTree mapTree;
+    private javax.swing.JButton testButton;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ farctool2/src/com/philosophofee/farctool2/MapParser.java
@@ -0,0 +1,147 @@
+package com.philosophofee.farctool2;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Enumeration;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+
+public class MapParser {
+
+    public DefaultTreeModel parseMapIntoMemory(TreeNode root, File file) {
+        DefaultTreeModel model = new DefaultTreeModel(root);
+
+        try {
+            long begin = System.currentTimeMillis();
+            MapParser self = new MapParser();
+            RandomAccessFile mapAccess = new RandomAccessFile(file, "rw");
+            int seek = 0;
+
+            //Read header
+            int header = mapAccess.readInt();
+            if (header == 256) {
+                System.out.println("LBP1/2 Map File");
+            }
+            if (header == 21496064) {
+                System.out.println("LBP3 Map File");
+            }
+            if (header != 256 && header != 21496064) {
+                throw new IOException("Error reading 4 bytes - not a valid .map file");
+            }
+            seek += 4;
+            mapAccess.seek(seek); //ok, were done!
+
+            //Read map entry count
+            int mapEntries = mapAccess.readInt();
+            System.out.println(mapEntries + "entries in file");
+            seek += 4;
+            mapAccess.seek(seek); //ok, were done!
+
+            //Read entry
+            int fileNameLength = 0;
+            String fileName = "";
+            int fileSize = 0;
+            String SHA1 = "";
+            int GUID = 0;
+
+            for (int i = 0; i < mapEntries; i++) {
+
+                //get filename length
+                fileNameLength = mapAccess.readInt();
+                //System.out.println("entry length=" + fileNameLength);
+                fileName = ""; // reset
+                seek += 4;
+                mapAccess.seek(seek);
+
+                //get filename string
+                for (int i2 = 0; i2 < fileNameLength; i2++) {
+                    fileName += (char) mapAccess.readByte();
+                    seek += 1;
+                    mapAccess.seek(seek);
+                }//for filename
+
+                //padding, 0x000000
+                seek += 4;
+                mapAccess.seek(seek);
+
+                //DATE, maybe fix later but unimportant now
+                seek += 4;
+                mapAccess.seek(seek);
+
+                //File size 4 bytes
+                fileSize = mapAccess.readInt();
+                seek += 4;
+                mapAccess.seek(seek);
+
+                //Hash 20 bytes
+                SHA1 = "";
+                for (int i3 = 0; i3 < 20; i3++) {
+                    SHA1 += String.format("%02X", mapAccess.readByte());
+                    seek += 1;
+                    mapAccess.seek(seek);
+                }
+                //GUID 4 bytes
+                GUID = 0;
+                GUID = mapAccess.readInt();
+                seek += 4;
+                mapAccess.seek(seek);
+                //self.addPath(fileName);
+                buildTreeFromString(model, fileName);
+                //System.out.println(fileName + " | size: " + fileSize + " | sha1: " + SHA1 + " | GUID: " + GUID);
+            }//for each map entry
+
+            //Close map
+            mapAccess.close();
+            long end = System.currentTimeMillis();
+            long timeTook = end - begin;
+
+            System.out.println("Map parsed in " + timeTook + "ms");
+            //self.printHtml(System.out);
+        } catch (IOException e) {
+            System.out.println("IOException:");
+            e.printStackTrace();
+        }
+        return model;
+    }
+
+    //public void loadMap(File file) {
+    //    parseMapIntoMemory(file);
+    //}
+    private void buildTreeFromString(final DefaultTreeModel model, final String str) {
+        DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
+
+        String[] strings = str.split("/");
+
+        DefaultMutableTreeNode node = root;
+
+        for (String s : strings) {
+            int index = childIndex(node, s);
+
+            if (index < 0) {
+                DefaultMutableTreeNode newChild = new DefaultMutableTreeNode(s);
+                node.insert(newChild, node.getChildCount());
+                node = newChild;
+            } else {
+                node = (DefaultMutableTreeNode) node.getChildAt(index);
+            }
+        }
+    }
+
+    private int childIndex(final DefaultMutableTreeNode node, final String childValue) {
+        Enumeration<DefaultMutableTreeNode> children = node.children();
+        DefaultMutableTreeNode child = null;
+        int index = -1;
+
+        while (children.hasMoreElements() && index < 0) {
+            child = children.nextElement();
+
+            if (child.getUserObject() != null && childValue.equals(child.getUserObject())) {
+                index = node.getIndex(child);
+            }
+        }
+
+        return index;
+    }
+}
